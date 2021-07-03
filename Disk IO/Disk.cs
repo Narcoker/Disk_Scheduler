30d2bc1a8b957959416b69e715b367bddc241fb3
Narcoker <jykim9868@gmail.com>
2021-07-03 16:22:07 +0900

    FCFS, SSTF, SCAN, LOOK, SPTF

---
+++ Disk IO/Disk.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disk_IO
+{
+    class Disk
+    {
+        List<int> DiskQueue;
+
+        public Disk()
+        {
+            this.DiskQueue = new List<int>();
+
+        }
+
+        public void EnqueueDiskQ(string args)
+        {
+            foreach (string tmp in args.Split(','))
+            {
+                this.DiskQueue.Add(int.Parse(tmp));
+            }
+        }
+
+        public void Clean()
+        {
+            this.DiskQueue.Clear();
+            Tracer.counter = 0;
+        }
+
+        public List<Tracer> SchedulingWithFCFS(int intialPoint)
+        {
+            List<Tracer> tr = new List<Tracer>();
+            int acc = 0;
+            int currentCur = intialPoint;
+            int movement = 0;
+
+            int num = this.DiskQueue.Count;
+            int responseSum = 0;
+
+            tr.Add(new Tracer(currentCur, 0, 0, 0));
+
+            while (this.DiskQueue.Count > 0)
+            {
+                int nextCur = this.DiskQueue[0];
+                this.DiskQueue.RemoveAt(0);
+
+                movement = nextCur - currentCur;
+                acc += Math.Abs(movement);
+                responseSum += acc;
+
+                currentCur = nextCur;
+                tr.Add(new Tracer(currentCur, movement, acc, 0));
+
+            }
+
+            for (int i = 1; i < tr.Count; i++)
+            {
+                Tracer temp = tr[i];
+                temp.deviation = temp.accumulateTime - (responseSum / (double)num);
+                tr[i] = temp;
+            }
+
+            return tr;
+        }
+
+
+        public List<Tracer> SchedulingWithSSTF(int intialPoint)
+        {
+            List<Tracer> tr = new List<Tracer>();
+            int acc = 0;
+            int currentCur = intialPoint;
+            int index = -1;
+            int movement = 0;
+
+            int num = this.DiskQueue.Count;
+            int responseSum = 0;
+
+            tr.Add(new Tracer(currentCur, 0, 0, 0));
+
+            DiskQueue.Sort();
+
+            for (int i = 0; i < this.DiskQueue.Count; i++)
+            {
+                if (this.DiskQueue[i] <= currentCur) index++;
+            }
+
+            if (index == -1) return tr;
+
+
+
+            while (this.DiskQueue.Count > 0)
+            {
+                int nextCur;
+
+                if (index == 0 || index == DiskQueue.Count - 1)
+                {
+                    nextCur = DiskQueue[index];
+                    this.DiskQueue.RemoveAt(index);
+                    if (index == DiskQueue.Count) index--;
+
+
+                }
+
+                else if (Math.Abs(currentCur - DiskQueue[index]) <= Math.Abs(currentCur - DiskQueue[index + 1])) //index가 더 작을경우
+                {
+                    nextCur = DiskQueue[index];
+                    this.DiskQueue.RemoveAt(index);
+                    if (index != 0) index--;
+
+                }
+
+                else
+                {
+                    nextCur = DiskQueue[index + 1];
+                    this.DiskQueue.RemoveAt(index + 1);
+                }
+
+                movement = nextCur - currentCur;
+                acc = acc + Math.Abs(movement);
+                responseSum += acc;
+
+                currentCur = nextCur;
+                tr.Add(new Tracer(currentCur, movement, acc, 0));
+            }
+
+            for (int i = 1; i < tr.Count; i++)
+            {
+                Tracer temp = tr[i];
+                temp.deviation = temp.accumulateTime - (responseSum / (double)num);
+                tr[i] = temp;
+            }
+
+            return tr;
+        }
+
+        public List<Tracer> SchedulingWithSCAN(int intialPoint)
+        {
+            List<Tracer> tr = new List<Tracer>();
+            int acc = 0;
+            int currentCur = intialPoint;
+            int index = -1;
+            bool direction = true; // true == 왼쪽으로 false == 오른쪽으로
+            int movement = 0;
+
+            int num = this.DiskQueue.Count;
+            int responseSum = 0;
+
+            tr.Add(new Tracer(currentCur, 0, 0, 0));
+
+            DiskQueue.Sort();
+
+            for (int i = 0; i < this.DiskQueue.Count; i++)
+            {
+                if (this.DiskQueue[i] <= currentCur) index++;
+            }
+
+
+            while (this.DiskQueue.Count > 0)
+            {
+                int nextCur;
+
+                if (index < 0)
+                {
+                    index = 0;
+                    direction = false;
+                    nextCur = 0; 
+                }
+
+                else if (index > this.DiskQueue.Count - 1)
+                {
+                    index = this.DiskQueue.Count - 1;
+                    direction = true;
+                    nextCur = 400;
+                }
+
+                else
+                {
+                    nextCur = this.DiskQueue[index];
+
+                    if (direction == true) { this.DiskQueue.RemoveAt(index--); }
+                    else { this.DiskQueue.RemoveAt(0); }
+
+                }
+
+                movement = nextCur - currentCur;
+                acc = acc + Math.Abs(movement);
+                responseSum += acc;
+
+                currentCur = nextCur;
+                tr.Add(new Tracer(currentCur, movement, acc, 0));
+
+            }
+
+            for (int i = 1; i < tr.Count; i++)
+            {
+                Tracer temp = tr[i];
+                temp.deviation = temp.accumulateTime - (responseSum / (double)num);
+                tr[i] = temp;
+            }
+
+            return tr;
+        }
+
+
+        public List<Tracer> SchedulingWithLOOK(int intialPoint)
+        {
+            List<Tracer> tr = new List<Tracer>();
+            int acc = 0;
+            int currentCur = intialPoint;
+            int index = -1;
+            bool direction = true; // true == 왼쪽으로 false == 오른쪽으로
+            int movement = 0;
+
+            int num = this.DiskQueue.Count;
+            int responseSum = 0;
+
+            tr.Add(new Tracer(currentCur, 0, 0, 0));
+
+            DiskQueue.Sort();
+
+            for (int i = 0; i < this.DiskQueue.Count; i++)
+            {
+                if (this.DiskQueue[i] <= currentCur) index++;
+            }
+
+            while (this.DiskQueue.Count > 0)
+            {
+                int nextCur;
+
+                if (index < 0)
+                {
+                    index = 0;
+                    direction = false;
+                    nextCur = this.DiskQueue[index];
+                    DiskQueue.RemoveAt(index);
+
+                }
+
+                else if (index > this.DiskQueue.Count - 1)
+                {
+                    index = this.DiskQueue.Count - 1;
+                    direction = true;
+                    nextCur = this.DiskQueue[index];
+                    DiskQueue.RemoveAt(index);
+                }
+
+                else
+                {
+                    nextCur = this.DiskQueue[index];
+
+                    if (direction == true) { this.DiskQueue.RemoveAt(index--); }
+                    else { this.DiskQueue.RemoveAt(0); }
+                }
+
+                movement = nextCur - currentCur;
+                acc = acc + Math.Abs(movement);
+                responseSum += acc;
+
+                currentCur = nextCur;
+                tr.Add(new Tracer(currentCur, movement, acc, 0));
+            }
+
+            for (int i = 1; i < tr.Count; i++)
+            {
+                Tracer temp = tr[i];
+                temp.deviation = temp.accumulateTime - (responseSum / (double)num);
+                tr[i] = temp;
+            }
+
+            return tr;
+        }
+
+        public List<Tracer> SchedulingWithSPTF(int intialPoint)
+        {
+            List<Tracer> tr = new List<Tracer>();
+            int acc = 0;
+            int currentCur = intialPoint;
+            int movement = 0;
+
+            int num = this.DiskQueue.Count;
+            int responseSum = 0;
+
+            int intialSec = 0;
+            int currentSec = intialSec;
+
+            tr.Add(new Tracer(currentCur, 0, 0, 0));
+
+            List<int> sector = new List<int>();
+            List<int> mvPLUSrL = new List<int>(); // 회전지연시간 계산 리스트
+
+            for (int i = 0; i < this.DiskQueue.Count; i++)
+            {
+                int sec = this.DiskQueue[i] % 8;
+                sector.Add(sec);
+            }
+
+            while (this.DiskQueue.Count > 0)
+            {
+                int RL = 0;
+
+                for (int i = 0; i < this.DiskQueue.Count; i++)
+                {
+                    RL = sector[i] - currentSec;
+
+                    if (RL < 0)
+                    {
+                        RL = RL + 8;
+                        mvPLUSrL.Add(Math.Abs(currentCur - this.DiskQueue[i]) + RL);
+                    }
+                    else
+                    {
+                        mvPLUSrL.Add(Math.Abs(currentCur - this.DiskQueue[i]) + RL);
+                    }
+                }
+
+                int min = mvPLUSrL.IndexOf(mvPLUSrL.Min());
+
+                int nextCur = this.DiskQueue[min];
+                this.DiskQueue.RemoveAt(min);
+
+                movement = nextCur - currentCur;
+                acc = acc + mvPLUSrL[min];
+                responseSum += acc;
+
+                mvPLUSrL.Clear();
+
+                currentCur = nextCur;
+                currentSec = sector[min];
+                sector.RemoveAt(min);
+
+
+                tr.Add(new Tracer(currentCur, movement, acc, 0));
+
+            }
+
+            for (int i = 1; i < tr.Count; i++)
+            {
+                Tracer temp = tr[i];
+                temp.deviation = temp.accumulateTime - (responseSum / (double)num);
+                tr[i] = temp;
+            }
+
+            return tr;
+        }
+    }
+}
---
+++ Disk IO/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Disk_IO
+{
+    public partial class Form1 : Form
+    {
+        Disk disk;
+
+        public Form1()
+        {
+            this.disk = new Disk();
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            this.listView1.Items.Clear();
+            disk.Clean();
+            this.disk.EnqueueDiskQ(this.tbQueueList.Text);
+            this.chtSchedule.Series[0].Points.Clear();
+            this.chtSchedule.Series[1].Points.Clear();
+
+
+            String type = comboBox1.SelectedItem.ToString();
+
+
+            if (type == "FCFS")
+            {
+                var t = this.disk.SchedulingWithFCFS(int.Parse(this.tbInital.Text));
+                TracerRead(t);
+                t.Clear();
+            }
+
+            else if (type == "SSTF")
+            {
+                var t = this.disk.SchedulingWithSSTF(int.Parse(this.tbInital.Text));
+                TracerRead(t);
+                t.Clear();
+            }
+            else if (type == "SCAN")
+            {
+                var t = this.disk.SchedulingWithSCAN(int.Parse(this.tbInital.Text));
+                TracerRead(t);
+                t.Clear();
+            }
+
+            else if (type == "LOOK")
+            {
+                var t = this.disk.SchedulingWithLOOK(int.Parse(this.tbInital.Text));
+                TracerRead(t);
+                t.Clear();
+            }
+
+            else if(type == "SPTF")
+            {
+                var t = this.disk.SchedulingWithSPTF(int.Parse(this.tbInital.Text));
+                TracerRead(t);
+                t.Clear();
+            }
+          
+
+            
+
+        }
+
+        private void TracerRead(List<Tracer> tr)
+        {
+            int total = 0;
+            int num = 0;
+            double dispersion = 0;
+            
+
+            foreach ( var tmp in tr )
+            {
+                var listViewItem = new ListViewItem(tmp.jobNumber.ToString());
+                listViewItem.SubItems.Add(tmp.jobPosition.ToString());
+                listViewItem.SubItems.Add(tmp.jobMovement.ToString());
+                listViewItem.SubItems.Add(tmp.accumulateTime.ToString());
+                listViewItem.SubItems.Add(tmp.deviation.ToString());
+                listView1.Items.Add(listViewItem);
+
+                this.chtSchedule.Series[0].Points.AddXY(tmp.jobPosition, tmp.jobNumber);
+                this.chtSchedule.Series[1].Points.AddXY(tmp.jobPosition, tmp.jobNumber);
+                total = tmp.accumulateTime;
+                dispersion += tmp.deviation * tmp.deviation;
+                num = tmp.jobNumber;
+            }
+            this.lbDebugText.Text = "총 이동거리: " + total;
+            this.label4.Text = "표준편차: " + Math.Round(Math.Sqrt(dispersion / num) , 2);
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+
+            int count = rd.Next(8, 20);
+            StringBuilder sb = new StringBuilder();
+
+            this.tbInital.Text = rd.Next(0, 400).ToString();
+            for ( int i = 0; i < count; i++ )
+            {
+                sb.Append(rd.Next(0, 400));
+                sb.Append(",");
+            }
+            sb[sb.Length-1] = '\0';
+            this.tbQueueList.Text = sb.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chtSchedule_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbQueueList_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
